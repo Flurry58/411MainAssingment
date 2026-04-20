98167ce5cf9f4c84f1ea356b78df6e2aa5a0f922
Logan <loganc556@gmail.com>
2026-04-20 16:26:13 -0400

    Fix booboo

--- src/math_core.rs
+++ src/math_core.rs
@@ -3,19 +3,19 @@ use crate::memory::RamMem;
 // Only put mathematical Operations here no operation here will edit the program
 // counter in ANY WAY
 
-pub fn Add(reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
+pub fn Add(ram: &RamMem, reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
     counter + 1
 }
 
-pub fn Mult(reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
+pub fn Mult(ram: &RamMem, reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
     counter + 1
 }
 
-pub fn Div(reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
+pub fn Div(ram: &RamMem, reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
     counter + 1
 }
 
 // Bitwise And
-pub fn Btand(reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
+pub fn Btand(ram: &RamMem, reg_a: usize, reg_b: usize, reg_c: usize, counter: u32) -> u32 {
     counter + 1
 }
